28bffeddb55df78497ea72c68ef82eb963fcd42a
Maxim Fedin <m-fedin@mail.ru>
2019-11-25 19:03:29 +0300

    Tests for negative values for Protobuf codec has been added.

--- src/test/scala/org/enterprisedlt/fabric/contract/codec/ProtobufCodecTest.scala
+++ src/test/scala/org/enterprisedlt/fabric/contract/codec/ProtobufCodecTest.scala
@@ -14,7 +14,7 @@ class ProtobufCodecTest extends FunSuite {
 
     case class TestClass()
 
-    test("Protobuf structure encoding/decoding works fine") {
+    test("Protobuf message encoding/decoding works fine") {
         val msg: TestMessage = TestMessage.newBuilder()
           .setStringValue("Hello World")
           .setIntValue(10)
@@ -26,14 +26,14 @@ class ProtobufCodecTest extends FunSuite {
         assert(msg == decoded)
     }
 
-    test("Unsupported class for non protobuf object works fine") {
+    test("Unsupported class for non-protobuf object works fine") {
 
         an[java.lang.Exception] should be thrownBy { // Ensure a particular exception type is thrown
             codec.decode[TestClass](new Array[Byte](0), classOf[TestClass])
         }
     }
 
-    test("Int structure encoding/decoding works fine") {
+    test("Positive Int encoding/decoding works fine") {
         val msg: Int = 100
         //
         val encoded = codec.encode[Int](msg)
@@ -42,7 +42,17 @@ class ProtobufCodecTest extends FunSuite {
         assert(msg == decoded)
     }
 
-    test("Byte structure encoding/decoding works fine") {
+    test("Negative Int encoding/decoding works fine") {
+        val msg: Int = -100
+        //
+        val encoded = codec.encode[Int](msg)
+        val decoded = codec.decode[Int](encoded, classOf[Int])
+        //
+        assert(msg == decoded)
+    }
+
+
+    test("Byte encoding/decoding works fine") {
         val msg: Byte = 123
         //
         val encoded = codec.encode[Byte](msg)
@@ -52,7 +62,7 @@ class ProtobufCodecTest extends FunSuite {
     }
 
 
-    test("Short structure encoding/decoding works fine") {
+    test("Positive Short encoding/decoding works fine") {
         val msg: Short = 2
         //
         val encoded = codec.encode[Short](msg)
@@ -61,7 +71,16 @@ class ProtobufCodecTest extends FunSuite {
         assert(msg == decoded)
     }
 
-    test("Char structure encoding/decoding works fine") {
+    test("Negative Short encoding/decoding works fine") {
+        val msg: Short = -2
+        //
+        val encoded = codec.encode[Short](msg)
+        val decoded = codec.decode[Short](encoded, classOf[Short])
+        //
+        assert(msg == decoded)
+    }
+
+    test("Char encoding/decoding works fine") {
         val msg: Char = 'X'
         //
         val encoded = codec.encode[Char](msg)
@@ -70,7 +89,7 @@ class ProtobufCodecTest extends FunSuite {
         assert(msg == decoded)
     }
 
-    test("Float structure encoding/decoding works fine") {
+    test("Positive Float encoding/decoding works fine") {
         val msg: Float = .1f
         //
         val encoded = codec.encode[Float](msg)
@@ -79,7 +98,16 @@ class ProtobufCodecTest extends FunSuite {
         assert(msg == decoded)
     }
 
-    test("Long structure encoding/decoding works fine") {
+    test("Negative Float encoding/decoding works fine") {
+        val msg: Float = -.1f
+        //
+        val encoded = codec.encode[Float](msg)
+        val decoded = codec.decode[Float](encoded, classOf[Float])
+        //
+        assert(msg == decoded)
+    }
+
+    test("Positive Long encoding/decoding works fine") {
         val msg: Long = 100000
         //
         val encoded = codec.encode[Long](msg)
@@ -88,7 +116,16 @@ class ProtobufCodecTest extends FunSuite {
         assert(msg == decoded)
     }
 
-    test("Double structure encoding/decoding works fine") {
+    test("Negative Long encoding/decoding works fine") {
+        val msg: Long = -100000
+        //
+        val encoded = codec.encode[Long](msg)
+        val decoded = codec.decode[Long](encoded, classOf[Long])
+        //
+        assert(msg == decoded)
+    }
+
+    test("Positive Double encoding/decoding works fine") {
         val msg: Double = 100000.12
         //
         val encoded = codec.encode[Double](msg)
@@ -97,5 +134,12 @@ class ProtobufCodecTest extends FunSuite {
         assert(msg == decoded)
     }
 
-
+    test("Negative Double encoding/decoding works fine") {
+        val msg: Double = -100000.12
+        //
+        val encoded = codec.encode[Double](msg)
+        val decoded = codec.decode[Double](encoded, classOf[Double])
+        //
+        assert(msg == decoded)
+    }
 }
